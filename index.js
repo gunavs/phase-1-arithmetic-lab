104721fb1c996b9efe6dc96b531ea707e671e1f7
sukanyagovindan <gsukanya@gmail.com>
2022-06-07 22:24:03 -0700

    Submit Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=2,num2=31;
+let multiply = num1* num2;
+let random = Math.floor(Math.random() * 10) +1;
+let num3= 5,num4 = 24;
+let mod = num4%num3;
+let set = [5,7,20]
+let max = Math.max(...set)
+
